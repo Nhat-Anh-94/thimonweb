93d4c1cd511e4be1463c22e950388fca7e81ac30
Nhat-Anh-94 <165351129+Nhat-Anh-94@users.noreply.github.com>
2024-06-14 20:24:41 +0700

    Add files via upload

---
+++ Thi van dap/script.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 10; i++) if (i % 2 == 0) console.log(i);
+
+var num1 = document.getElementbyId(num1);
+var num2 = document.getElementbyId(num2);
+function compare(num1, num2) {
+  if (num1 === num2) {
+    console.log("Trả về giống nhau");
+  } else {
+    cosole.log("Trả về khác nhau");
+  }
+}
